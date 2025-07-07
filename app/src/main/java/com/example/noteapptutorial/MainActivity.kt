b94192688d2de979bca6c67afac2f3245dee4b4a
JeinJang <jeinjang99@gmail.com>
2025-07-07 16:00:55 +0900

    RequiresApi annotation

--- app/src/main/java/com/example/noteapptutorial/MainActivity.kt
+++ app/src/main/java/com/example/noteapptutorial/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.noteapptutorial
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.noteapptutorial.ui.NoteCanvas
 
+@RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +23,7 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
+@RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
 fun NotePreview() {
     NoteCanvas()
 }
